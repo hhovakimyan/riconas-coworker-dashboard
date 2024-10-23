d7bccb135dfea9202b167ffa3ce8a8663c21d528
hrayr <developer.hovakimyan@gmail.com>
2024-10-23 14:19:21 +0400

    Fix house connections page responsiveness

--- src/pages/HouseConnectionPage/MontageJobsList/styles.ts
+++ src/pages/HouseConnectionPage/MontageJobsList/styles.ts
@@ -1,3 +1,5 @@
+import { sidebarWidth } from 'constants/main';
+
 export const multiValueCellStyles = {
   padding: '10px !important',
   '& .MuiTypography-root': {
@@ -7,4 +9,5 @@ export const multiValueCellStyles = {
 
 export const tableContainerStyles = {
   maxHeight: '80vh',
+  maxWidth: `calc(100vw - ${sidebarWidth + 60}px)`,
 }
--- src/pages/HouseConnectionPage/styles.ts
+++ src/pages/HouseConnectionPage/styles.ts
@@ -2,6 +2,4 @@ import { sidebarWidth } from 'constants/main';
 
 export const mainContentStyles = {
   marginLeft: `${sidebarWidth}px`,
-  overflowX: 'scroll',
-  maxWidth: `calc(100% - ${sidebarWidth}px)`
 }
