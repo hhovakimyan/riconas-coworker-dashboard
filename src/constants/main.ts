561ee0f4ad389cb8bc1e9eba0eb4887ecc0a6512
hrayr <developer.hovakimyan@gmail.com>
2024-10-25 12:21:34 +0400

    Add "blow_in" view type

--- src/constants/main.ts
+++ src/constants/main.ts
@@ -9,9 +9,12 @@ export const PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@
 
 export const VIEW_TYPE_MOUNTING = 'mounting';
 
+export const VIEW_TYPE_BLOW_IN = 'blow_in';
+
 export const VIEW_TYPES = [
   'overview',
   VIEW_TYPE_MOUNTING,
+  VIEW_TYPE_BLOW_IN,
 ];
 
 // This is the maximum size of all files which are being uploaded in one chunk
--- src/pages/HouseConnectionPage/index.tsx
+++ src/pages/HouseConnectionPage/index.tsx
@@ -3,9 +3,10 @@ import { useState } from 'react';
 
 import Sidebar from 'components/Sidebar';
 import { mainContentStyles } from 'pages/HouseConnectionPage/styles';
-import { VIEW_TYPE_MOUNTING } from 'constants/main';
+import { VIEW_TYPE_BLOW_IN, VIEW_TYPE_MOUNTING } from 'constants/main';
 import MontageJobsList from 'pages/HouseConnectionPage/MontageJobsList';
 import { SidebarFilterProps } from 'types/generic';
+import BlowInJobsList from 'pages/HouseConnectionPage/BlowInJobsList';
 
 const HouseConnectionPage = () => {
   const [viewType, setViewType] = useState<string>(VIEW_TYPE_MOUNTING);
@@ -22,6 +23,7 @@ const HouseConnectionPage = () => {
       />
       <Box sx={mainContentStyles}>
         {viewType === VIEW_TYPE_MOUNTING && <MontageJobsList sidebarFilter={sidebarFilter} /> }
+        {viewType === VIEW_TYPE_BLOW_IN && <BlowInJobsList sidebarFilter={sidebarFilter} /> }
       </Box>
     </Box>
   );
