37d7b402b9fa38ef1929b1e1b7c4201a4baa9fa7
hrayr <developer.hovakimyan@gmail.com>
2024-11-19 18:23:18 +0400

    Enhance PageTitle component tests

--- src/components/PageTitle/index.test.tsx
+++ src/components/PageTitle/index.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 
 import PageTitle from 'components/PageTitle/index';
 
-it('Component renders correctly', () => {
+it('Component renders correctly when className is passed in props', () => {
   const title = 'Hello World';
   const titleClassName = 'myClass';
 
@@ -12,4 +12,15 @@ it('Component renders correctly', () => {
 
   expect(titleElement).toBeInTheDocument();
   expect(titleElement.className).toBe(titleClassName);
+});
+
+it('Component renders correctly when className is not passed in props', () => {
+  const title = 'Hello World';
+
+  render(<PageTitle>{title}</PageTitle>);
+
+  const titleElement = screen.getByText(title);
+
+  expect(titleElement).toBeInTheDocument();
+  expect(titleElement.className).toBeFalsy();
 });
